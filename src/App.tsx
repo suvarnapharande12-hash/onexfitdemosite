ae0ca49ba1c4540f0b8ab3b32123f9b4c359aec0
suvarnapharande12-hash <suvarna.pharande12@gmail.com>
2026-06-01 22:18:00 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>ONEXFIT Health Club</h1>
+      <p>Premium Functional Training Studio in Pune</p>
+      <button>Book Free Trial</button>
+    </div>
+  )
+}
