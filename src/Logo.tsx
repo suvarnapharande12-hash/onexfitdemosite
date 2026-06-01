4d8c14c59903a3928e208d4b99cb36e69a97123e
suvarnapharande12-hash <suvarna.pharande12@gmail.com>
2026-06-01 22:14:13 +0530

    Create Logo.tsx

---
+++ src/Logo.tsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+  return (
+    <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="128" cy="112" r="92" fill="gold" />
+      <text x="50%" y="50%" textAnchor="middle" fill="black" fontSize="24">
+        ONEXFIT
+      </text>
+    </svg>
+  )
+}
