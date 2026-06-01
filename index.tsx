a39bb5d8329a478123e8aae8e472a94006896636
suvarnapharande12-hash <suvarna.pharande12@gmail.com>
2026-06-01 22:11:48 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,22 +1,9 @@
-<svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <defs>
-    <linearGradient id="gold" x1="32" y1="28" x2="224" y2="228" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#F6E2A6"/>
-      <stop offset="0.42" stop-color="#D8B45C"/>
-      <stop offset="0.72" stop-color="#C89A3C"/>
-      <stop offset="1" stop-color="#8F6A25"/>
-    </linearGradient>
-    <radialGradient id="glow" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(128 112) rotate(90) scale(112)">
-      <stop stop-color="#F3DD9A" stop-opacity="0.35"/>
-      <stop offset="1" stop-color="#F3DD9A" stop-opacity="0"/>
-    </radialGradient>
-  </defs>
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
-  <circle cx="128" cy="112" r="92" fill="url(#glow)"/>
-
-  <path d="M60 58C70 44 84 34 101 30L129 79L157 30C174 34 188 44 198 58L149 111L206 173C197 188 183 200 166 206L128 147L90 206C73 200 59 188 50 173L107 111L60 58Z" fill="url(#gold)"/>
-  <path d="M86 70L121 106L86 142" stroke="#2A1E0A" stroke-width="14" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M170 70L135 106L170 142" stroke="#2A1E0A" stroke-width="14" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M113 112H143" stroke="#2A1E0A" stroke-width="13" stroke-linecap="round"/>
-  <circle cx="128" cy="24" r="12" fill="url(#gold)"/>
-</svg>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
